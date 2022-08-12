97d6ea79fa7853be53455125b9ec6700dbe885ac
riley soloner <rileysoloner@gmail.com>
2022-08-12 15:50:10 -0400

    commit tree fbfb545aa5a0219ce0391228f35d73b4fc82992a

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
 
+  def name=(input)
+    @name = input
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(breed_input)
+    @breed = breed_input
+  end
+
+  def breed
+    @breed
+  end
+
 end
